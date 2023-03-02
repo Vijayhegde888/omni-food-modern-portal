e79f6125c7b43b7b190aee351774a1d1c7347465
Vijayhegde888 <vijayhegde888@gmail.com>
2023-03-02 22:42:24 +0530

    final change before deploy

--- js/script.js
+++ js/script.js
@@ -9,3 +9,21 @@ const headerEl=document.querySelector(".header")
 butnNav.addEventListener("click",function(){
  headerEl.classList.toggle("nav-open")   
 })
+
+// Sticky navigation 
+const sectionHeroEl= document.querySelector(".section-hero")
+const obs = new IntersectionObserver(function(entries){
+    const ent=entries[0];
+    console.log('ent',ent)
+    if(!ent.isIntersecting)
+    {
+
+        document.querySelector(".header").classList.add("sticky")
+    }
+},{
+    
+    root:null,
+    threshold:0
+})
+
+obs.observer(sectionHeroEl);
